0bfc41031cb23c7525f231a984a99978ed431e36
selfint <selfint@gmail.com>
2023-01-02 23:38:45 +0200

    implement then

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+trait Expression {
+    fn then(self, next: impl Into<String>) -> String;
+}
+
+impl Expression for &str {
+    fn then(self, next: impl Into<String>) -> String {
+        Into::<String>::into(self) + &next.into()
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +13,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_then() {
+        let expected = "ab";
+
+        let actual_str = "a".then("b");
+        let actual_string_1 = "a".to_string().then("b");
+        let actual_string_2 = "a".then("b".to_string());
+        let actual_string_3 = "a".to_string().then("b".to_string());
+
+        assert_eq!(expected, actual_str);
+        assert_eq!(expected, actual_string_1);
+        assert_eq!(expected, actual_string_2);
+        assert_eq!(expected, actual_string_3);
     }
 }
