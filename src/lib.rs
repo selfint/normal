e7de4eab06e1507927240731e1cb91b72fb4e274
selfint <selfint@gmail.com>
2023-01-03 12:50:52 +0200

    add constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
-trait Expression {
+pub const DIGIT: &str = "\\d";
+pub const NON_DIGIT: &str = "\\D";
+pub const WHITESPACE: &str = "\\s";
+pub const NON_WHITESPACE: &str = "\\S";
+pub const WORD: &str = "\\w";
+pub const NON_WORD: &str = "\\W";
+pub const WORD_BOUNDARY: &str = "\\b";
+pub const NON_WORD_BOUNDARY: &str = "\\B";
+pub const NEWLINE: &str = "\\n";
+pub const NON_NEWLINE: &str = ".";
+
+pub const START: &str = "^";
+pub const START_FORCED: &str = "\\A";
+pub const END: &str = "$";
+pub const END_FORCED: &str = "\\Z";
+
+pub const START_OF_MATCH: &str = "\\G";
+
+pub const CARRIAGE_RETURN: &str = "\\r";
+pub const TAB: &str = "\\r";
+pub const NULL: &str = "\\0";
+pub const BACKSPACE: &str = "[\\b]";
+pub const ESCAPE: &str = "\\e";
+
+pub trait Expression {
     fn then(self, next: impl Into<String>) -> String;
     fn or(self, other: impl Into<String>) -> String;
     fn then_repeated(self, next: impl Into<String>) -> String;
